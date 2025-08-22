74045b49af91d5b55c85ea0adab864cb500230d7
labsams71-dev <labsams71@gmail.com>
2025-08-22 17:50:28 +0200

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BLE_Sniffer",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(
+            name: "BLE_Sniffer",
+            targets: ["BLE_Sniffer"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "BLE_Sniffer",
+            dependencies: [],
+            path: "BLE_Sniffer",
+            sources: [
+                "BLE_SnifferApp.swift",
+                "ContentView.swift", 
+                "BLEManager.swift",
+                "BLEDevice.swift",
+                "SniffView.swift",
+                "LibraryView.swift",
+                "DeviceDetailView.swift"
+            ],
+            resources: [
+                .process("Assets.xcassets"),
+                .process("Info.plist")
+            ]),
+    ]
+) 
